26d36cca32c7ceab9f1bf92cbe5f0f0dd6ce8077
Muxammadali1998 <turgunaliyev.98@gmail.com>
2024-03-20 19:58:46 +0500

    addVoyager

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -18,9 +18,14 @@ class TelegramController extends Controller
     public function hendel(Request $request)
     {
         $input = $request->all();
+
+
+        dd($input);
+        if(isset($input['message'])){
+
         $message = $input['message'];
         $chat_id = $message['chat']['id'];
-        $text = $message['text']??'';
+        $text = $message['text']??null;
         $contact = $message['contact']??false;
 
         $user = Client::where('chat_id',$message['from']['id'])->first();
@@ -35,7 +40,7 @@ class TelegramController extends Controller
 
 
         if($contact){
-          
+
             $user->update(["phone_number" => $contact['phone_number'], 'status'=>3]);
 
             $this->telegram->call('sendMessage', [
@@ -97,33 +102,73 @@ class TelegramController extends Controller
 
         }else {
 
-            if (is_null($user->full_name)){
+           if (empty($text) and !is_null($user->phone_number)){
+                $this->telegram->call('sendMessage', [
+                    'chat_id' => $chat_id,
+                    'text' => "⏳",
+                ]);
+            }
+            elseif (is_null($user->full_name) and !empty($text)){
                 $user->update(['full_name'=>$text,'status'=>2]);
+            }elseif (empty($text) and empty($user->full_name)){
+               $user->update(['status'=>1]);
+
+               $this->telegram->call('sendMessage', [
+                   'chat_id' => $chat_id,
+                   'text' => "Ismingizni kiriting",
+               ]);
+           }
+
+            if (is_null($user->phone_number) and !empty($user->full_name)){
+
+                $requestContactButton = [
+                    [
+                        'text' => 'Telefon raqamini yuborish',
+                        'request_contact' => true
+                    ]
+                ];
+
+                $button = [$requestContactButton];
+
+                $keyboard = json_encode([
+                    'keyboard' => $button,
+                    'resize_keyboard' => true,
+
+                ]);
+
+
+                $this->telegram->call('sendMessage', [
+                    'chat_id' => $chat_id,
+                    'text' => "Ro'yxatdan otish uchun telefon raqamini tugma yordamida yuboring",
+                    'reply_markup'=>$keyboard
+                ]);
+
+
+            }elseif(!empty($text) and !empty($user->full_name)){
+
+                $this->telegram->call('sendMessage', [
+                    'chat_id' => $chat_id,
+                    'text' => "Botga tahrirlangan surat yuboring",
+                ]);
             }
 
-            $requestContactButton = [
-                [
-                    'text' => 'Telefon raqamini yuborish',
-                    'request_contact' => true
-                ]
-            ];
-
-            $button = [$requestContactButton];
-
-            $keyboard = json_encode([
-                'keyboard' => $button,
-                'resize_keyboard' => true,
-
-            ]);
-
-
-            $this->telegram->call('sendMessage', [
-                'chat_id' => $chat_id,
-                'text' => "Ro'yxatdan otish uchun telefon raqamini tugma yordamida yuboring",
-                'reply_markup'=>$keyboard
-            ]);
         }
     }
+        }elseif(isset($input['my_chat_member']) and false){
+            $message = $input['my_chat_member'];
+
+            $user = Client::where('chat_id',$message['from']['id'])->first();
+            if(!$user){
+                $user =  Client::create([
+                    'chat_id'=>$message['chat']['id'],
+                    'nik_name'=>$message['chat']['first_name']
+                ]);
+            }
+            $this->telegram->call('sendMessage', [
+                'chat_id' => $message['chat']['id'],
+                'text' => "https://t.me/clear_face_bot",
+            ]);
+        }
 
 
     }
